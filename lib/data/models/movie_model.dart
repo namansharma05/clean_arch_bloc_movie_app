4b9633dff2930c53e73ed76c487820dbc74727f4
Naman Sharma <namanks0511@gmail.com>
2024-09-17 01:10:18 -0700

    added movie_model file in models of data layers

---
+++ lib/data/models/movie_model.dart
@@ -0,0 +1,33 @@
+class MovieModel {
+  final int id;
+  final String title;
+  final String overview;
+  final String posterPath;
+
+  MovieModel({
+    required this.id,
+    required this.title,
+    required this.overview,
+    required this.posterPath,
+  });
+
+  // Converts JSON into MovieModel
+  factory MovieModel.fromJson(Map<String, dynamic> json) {
+    return MovieModel(
+      id: json['id'],
+      title: json['title'],
+      overview: json['overview'],
+      posterPath: json['posterPath'],
+    );
+  }
+
+  // Converts MovieModel into JSON
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'overview': overview,
+      'posterPath': posterPath,
+    };
+  }
+}
